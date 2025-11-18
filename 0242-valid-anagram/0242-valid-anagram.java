1630c85b7878076924ff57ae654324f8ecd65a21
Samiksha <112839465+samikshaasodekar@users.noreply.github.com>
2025-11-18 14:25:45 +0530

    Time: 6 ms (43.11%), Space: 43.9 MB (65.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,14 +3,20 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
+
         int[] count = new int[26];
         for(int i=0; i<s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
         }
-        for (int c : count) {
-            if (c != 0) return false;
+        for(int value : count){
+            if(value != 0){
+                return false;
+            }
         }
         return true;
     }
 }
+
+//TC:O(n+m)
+//SC:O(1)since we have at most 26 different characters
